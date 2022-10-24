24b1a166963d6705d343370edd7f755a143a026b
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-10-24 12:29:32 -0300

    implementado a tela home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Text } from './styles';
+
+function Home(){
+  return (
+    <Container>
+      <Text>Meu componente</Text>
+    </Container>
+  );
+}
+
+export default Home;
