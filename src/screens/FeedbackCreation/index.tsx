226740b5594f243a1047a63a6a8863f0f6bdc801
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-10-26 16:55:47 -0300

    finalizado tela de feedback da adição de refeição

--- src/screens/FeedbackCreation/index.tsx
+++ src/screens/FeedbackCreation/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
-import { Container, Title } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
-function FeebackCreation() {
+import feedbackNeg from '@assets/feedbackNeg.png';
+import feedbackPos from '@assets/feedbackPos.png';
+
+import { Container, Image, SubTitle, Title, Wrapper } from './styles';
+
+import AddMeal from '@components/AddMeal';
+
+type Props = {
+  onDiet: boolean;
+}
+
+function FeebackCreation({onDiet}: Props) {
+  const navigation = useNavigation();
+
+  function handleNavigation(path: any) {
+    if (path) {
+      navigation.navigate(path);
+    } else {
+      navigation.goBack();
+    }
+  }
+
   return (
     <Container>
-      <Title>Meu componente</Title>
+      <Title>{onDiet ? 'Continue Assim!' : 'Que Pena!'}</Title>
+      <SubTitle>
+        {
+          onDiet
+          ? 'Você continua dentro da dieta. Muito bem!'
+          : 'Você saiu da dieta dessa vez, mas continue se esforçando e não desista!'
+        }
+      </SubTitle>
+      <Image source={onDiet ? feedbackPos : feedbackNeg} />
+
+      <Wrapper>
+        <AddMeal
+          title='Ir para página inicial'
+          showIcon={false}
+          onPress={() => handleNavigation('home')}
+        />
+      </Wrapper>
     </Container>
   );
 }
--- src/screens/FeedbackCreation/styles.ts
+++ src/screens/FeedbackCreation/styles.ts
@@ -1,19 +1,43 @@
 import styled from 'styled-components/native'
 
+type Props = {
+  onDiet: boolean;
+}
+
 export const Container = styled.View`
   align-items: center;
   flex: 1;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
 `;
 
-export const Title = styled.Text`
-  color: red;
-  font-size: 16px;
-  font-weight: bold;
+export const Title = styled.Text<Props>`
+  color: ${({ theme, onDiet }) => onDiet ? theme.COLORS.GREEN_500 : theme.COLORS.RED_DARK};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-weight: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+
+  margin-bottom: 10px;
 `;
 
-export const subTitle = styled.Text`
-  color: red;
-  font-size: 16px;
-  font-weight: bold;
+export const SubTitle = styled.Text`
+  color:  ${({ theme }) => theme.COLORS.BROWN_700 };
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-weight: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+  width: 70%;
+
+  text-align: center;
+
+  margin-bottom: 20px;
 `;
+
+export const Image = styled.Image`
+  width: 300px;
+  height: 400px;
+`
+
+export const Wrapper = styled.View`
+  margin-top: 20px;
+  width: 70%;
+`
